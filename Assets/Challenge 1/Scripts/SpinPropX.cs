97749233a8f3a21eedddbe16b109d589c67094a3
Mikael <cmito004@edu.xamk.fi>
2020-11-02 11:45:16 +0200

    Add everything

---
+++ Assets/Challenge 1/Scripts/SpinPropX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropX : MonoBehaviour
+{
+    //Definte the rotation speed of the propeller
+    float rotationSpeed = 3000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate the propeller
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+    }
+}
